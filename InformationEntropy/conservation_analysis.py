1e6b45143187072c390f5d98b5c6ff68a0b99172
daniellemiller <danimillers10@gmail.com>
2019-11-09 13:56:02 +0200

    full analysis GMM plus rank to train data

--- InformationEntropy/conservation_analysis.py
+++ InformationEntropy/conservation_analysis.py
@@ -196,6 +196,51 @@ def get_median_conservation_score(start, end, seq_id, alias):
     median_conv = conv[conv['position'].isin(list(range(start, end+1)))]['score_bin'].median()
     return median_conv
 
+def generate_list_of_sequetial_positions(drops):
+    """
+    create a list of positions [start_i, end_i for i in drops]
+    :param drops: a data frame of a single sequence drops information
+    :return: a list of all positions
+    """
+    l_start = drops['start'].values
+    l_end = drops['end'].values
+
+    l_combined = []
+    for i in range(len(l_start)):
+        l_combined.append(l_start[i])
+        l_combined.append(l_end[i])
+    return np.array(l_combined)
+
+def get_drop_id_by_pos(pos, drops):
+    """
+
+    :param pos:
+    :param drops:
+    :return:
+    """
+
+    all_drop_positions = generate_list_of_sequetial_positions(drops)
+    pos_idx = all_drop_positions.searchsorted(pos)
+
+    # check if its between drops or within drop
+    set_start = -1
+    set_end = -1
+
+    if pos_idx % 2 != 0: # within drop
+        set_start = all_drop_positions[pos_idx - 1]
+        set_end = all_drop_positions[pos_idx + 1]
+    else:
+        if pos in all_drop_positions:
+            set_start = pos
+            set_end = all_drop_positions[pos_idx + 1]
+
+    if set_start == -1:
+        return "no drop"
+    else:
+        return drops[(drops['start'] == set_start) & (drops['end'] == set_end)]['drop_id'].values[0]
+
+
+
 
 def merge_drops_n_conservation(alias, drop_type):
     """
@@ -221,16 +266,57 @@ def merge_drops_n_conservation(alias, drop_type):
     drops = pd.read_csv(drops)
     drops = drops[drops['type'] == drop_type]
     drops['seq_id'] = drops['seq'].apply(lambda x: mapping[x])
+
+    # remove duplicated seq_i rows
+    ids_2_consider = []
+    seq_id_used = []
+    for key in mapping.keys():
+        v = mapping[key]
+        if v in seq_id_used:
+            continue
+        else:
+            seq_id_used.append(v)
+            ids_2_consider.append(key)
+    drops = drops[drops['seq'].isin(ids_2_consider)]
+
     drops['median_conservation'] = drops.apply(lambda row: get_median_conservation_score(row['start'], row['end'],
                                                                                          row['seq_id'], alias), axis = 1)
 
+    # add the drop identifier
+    indices = ["Drop {}".format(i) for i in drops.index.values]
+    drops['drop_id'] = indices
+
+    # merge data with train matrix for each id separately
+    for seq_id in drops['seq_id'].unique():
+        train_data = pd.read_csv(os.path.join(train, "input_mat_{}.csv".format(seq_id)))
+        cur_drop = drops[drops['seq_id'] == seq_id]
+
+        train_data['drop_id'] = train_data['pos'].apply(lambda x: get_drop_id_by_pos(x, cur_drop))
+
+        train_drops = train_data[train_data['drop_id'] != 'no drop']
+
+        # now we have a matrix containing only drops for each sequence, with their identifier.
+        # fit a GMM model to all positions remained, including probabilities and calculate the rank of each drop
+        gmm_clusterd = fit_GMM(train_drops[cols_2_consider])
+        merged = pd.merge(gmm_clusterd, train_drops[['position', 'seq_id', 'drop_id']])
+
+        # add ranks to each drop.
+        score_2_cluster = []
+        for cluster in ["0", "1", "2", "3"]:
+            score_2_cluster.append(tuple(cluster, cluster_score(merged[merged['GMM_clusters'] == cluster])))
+
+        ranking = score_to_rank(score_2_cluster)
+        merged['rank'] = merged['GMM_clusters'].apply(lambda x: ranking[x])
+        merged.to_csv(os.path.join(train, '{}_clustered_and_ranked.csv'.format(alias)))
 
 
-    files = glob.glob(os.path.join(train, 'input_*.csv'))
-    for f in files:
-        df = pd.read_csv(f)
-        df = df[cols_2_consider]     # entropies
-        id_2_seq = [k for k, v in mapping if k[v] == alias][0] # choose the first seq arbitrarily
+
+
+
+
+
+
+
 
 
 
